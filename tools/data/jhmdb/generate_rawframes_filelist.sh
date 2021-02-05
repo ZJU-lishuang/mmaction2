cf95d3fb3fcbbf33ca0955093a0149a98fe70aaa
lishuang <qqlishuang@gmail.com>
2021-02-05 17:16:10 +0800

    minor fix

--- tools/data/jhmdb/generate_rawframes_filelist.sh
+++ tools/data/jhmdb/generate_rawframes_filelist.sh
@@ -5,4 +5,4 @@ cd ../../../
 PYTHONPATH=. python tools/data/build_file_list.py jhmdb data/jhmdb/Frames/ --level 2 --format rawframes --rgb-prefix 0 --shuffle
 echo "Filelist for rawframes generated."
 
-cd tools/data/hmdb51/
+cd tools/data/jhmdb/
